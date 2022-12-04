147e7a04ac87be81d911eee93fbcdbbff6220b15
Lucas Otero <lucasote12@gmail.com>
2022-12-04 20:42:05 -0300

    Update App.kt

--- src/main/kotlin/notificationservice/App.kt
+++ src/main/kotlin/notificationservice/App.kt
@@ -3,6 +3,7 @@ package notificationservice
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @OpenAPIDefinition(info = Info(
@@ -14,11 +15,7 @@ class App {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println("Hello World!")
-
-            // Try adding program arguments via Run/Debug configuration.
-            // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-            println("Program arguments: ${args.joinToString()}")
+            runApplication<App>(*args)
         }
     }
 }
